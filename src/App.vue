c2babb816459eeed4ea24679d1ef4dc347e60b75
2406Bilal <bilolismonov6@gmail.com>
2023-10-17 02:41:27 -0700

    first time

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SignupComponents />
+</template>
+
+<script>
+import SignupComponents from './components/signup_components.vue'
+
+export default {
+  name: 'App',
+  components: { SignupComponents }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
+}
+</style>
+<!-- Пустая строка после закрывающего тега </style> -->
